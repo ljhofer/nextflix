28ee1fd8d8cc0a8199b734a0f140c1e63a457b96
Laura Hofer <ljhofer@gmail.com>
2021-10-03 13:45:16 -0500

    Created event listeners for click on search button and click on favorites

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,65 @@
-console.log("Hello world!");
+// Variablees for search area
+var searchBtn = $("#");
+var searchInputField = $("#")[0];
 
-console.log("Test this");
+// Variables for results/popular movies area
+resultsDiv = $("");
+
+//Global variables
+var favoriteList = [];
+
+
+
+
+// Start function
+	// Pull popular query from API
+	// Push content to cards
+
+	
+// function getAPI
+
+	//(code is in practice js file)//copy error text from homework
+	// Call populateCards function pass data
+
+
+// function populateCards
+	// Create individual variables for data
+	// Iterate over 4 existing cards to populate with fetch results
+		// Title, year, overview
+
+// function saveFavorites
+	// Check for existing favorites from local storage
+		// Set variable === JSON parse local storage
+		// Stringify local storage
+
+
+// Calls start function
+startPage();
+
+// Event listener for click on search button
+searchBtn.click(function(event) {
+    event.preventDefault();
+    currentSearch = searchInputField.value;
+    getAPI(currentSearch);
+})
+
+// Event listener on each card or on card div for click to add to favorites
+	// Check to see if title exists in local storage
+		// If no, save movie title to JSON object of favorite movies 
+		// If yes, link to favorites page
+
+resultsDiv.click(function(event){
+    // TODO: make this this event target works
+    var newFavorite = event.target
+    
+    if (localStorage.getItem("favoriteList") !== null) {
+        favoriteList = JSON.parse(localStorage.getItem("favoriteList"));
+    }
+
+    if (!previousCities.includes(currentSearch)) {
+         // Adds movie to favorites
+         previousCities.push(newFavorite);
+         localStorage.setItem("favoriteList", JSON.stringify(favoriteList));
+     }
+
+})
